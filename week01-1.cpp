f90182eb54e0f233e9f75679a7d36f68201803cb
XiaoBai1221aaa <112918570+XiaoBai1221aaa@users.noreply.github.com>
2022-09-13 11:26:09 +0800

    Week01-1.cpp

---
+++ week01-1.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+ printf("Hello World\n");
+ printf("Hello World\n");
+ printf("我是小白\n");
+ ///有時候中文會有問題,你可以用英文
+ //註解 comments 是給人看的 作業二加註解
+ ///CodeBlocks如果畫三條,會比較深
+}
