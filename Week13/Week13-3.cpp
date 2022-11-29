c21aa1bc6df735f7915a55bf7aa1a4f4bfadbbf0
XiaoBai1221aaa <lawrens.pai@gmail.com>
2022-11-29 12:04:05 +0800

    修改目錄

---
+++ Week13/Week13-3.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void display_num( int a )
+{
+    printf("你丟給display_num()的參數引數是: %d\n",a);
+    return;///如果是void可以不用寫return
+}
+int main()
+{
+    display_num(100);
+    display_num(200);
+}
