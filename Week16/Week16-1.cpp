792c424a924f2a08348258a2297207d0b2b80c51
XiaoBai1221aaa <lawrens.pai@gmail.com>
2022-12-20 11:55:55 +0800

    Week16

---
+++ Week16/Week16-1.cpp
@@ -0,0 +1,15 @@
+///狡策 scanf()
+#include <stdio.h>
+///ρ畍量秆stdio.h琌或
+
+int main()
+{
+    printf("叫块计,ダ\n");
+    int a;
+    scanf("%d",&a);
+
+    char c;
+    scanf("%c", &c);
+
+    printf("计琌:%d ダ琌:%c",a,c);
+}
