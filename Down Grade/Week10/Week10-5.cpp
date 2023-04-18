979a3f5834d1d4c4490f5b6f1e7d0d64a088e7a4
XiaoBai1221aaa <lawrens.pai@gmail.com>
2023-04-18 11:53:03 +0800

    Week10

---
+++ Down Grade/Week10/Week10-5.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        //Study Plan 的 LeetCode 75的 Day 1 的 1480. Running Sum of 1d Array
+        //從1開始,因為要看到前一格nums[i-1]
+        for(int i=1;i<nums.size();i++){
+            nums[i] = nums[i] + nums[i-1];
+            //每一格,就是把前一格,加進來
+        }
+        return nums;
+    }
+};
